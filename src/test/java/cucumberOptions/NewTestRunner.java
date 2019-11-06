48e5034a5ada2ab2b68e1b56cf2fad50b489288e
etisachin <vicky21bhandari@gmail.com>
2019-11-06 13:41:48 +0530

    commit by master dev

---
+++ src/test/java/cucumberOptions/NewTestRunner.java
@@ -0,0 +1,18 @@
+package cucumberOptions;
+
+import org.testng.annotations.Test;
+
+public class NewTestRunner {
+	
+	
+	
+	@Test
+	public void test()
+	
+	{
+		
+	System.out.println("asda test ");	
+		
+	}
+
+}
